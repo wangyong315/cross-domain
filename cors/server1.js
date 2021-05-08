ff6087fa25eb7e3c63e6b71417c04bdd710adb92
wangyong315 <291140738@qq.com>
2021-05-08 21:13:22 +0800

    fix：实现cors跨域

--- cors/server1.js
+++ cors/server1.js
@@ -9,11 +9,23 @@ app.use(function (req, res, next) {
   if (whiteList.includes(origin)) {
     // 设置那个源可以访问我
     res.setHeader('Access-Control-Allow-Origin', origin)
+    // res.setHeader('Access-Control-Allow-Headers', 'name')
+    res.setHeader('Access-Control-Allow-Methods', 'PUT')
+    if(req.method === 'OPTIONS'){
+      console.log('OPTIONS', req.method);
+      res.end()
+    }
   }
   next()
 })
 
 app.use(express.static(__dirname))
+
+app.put('/getData', (req, res) => {
+  console.log('req', req.headers);
+  res.end('我不爱你')
+})
+
 
 app.get('/getData', (req, res) => {
   console.log('req', req.headers);
